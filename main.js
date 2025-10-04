4a96fe78023e0918b8691fb1f368e7d3de5a2e9f
kakutasoma <kakusoma0418@gmail.com>
2025-10-04 23:51:58 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+{
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    const overlay = document.querySelector('.overlay');
+
+    open.addEventListener('click', () => {
+        overlay.classList.add('show');
+        open.classList.add('hidde');
+    });
+
+    close.addEventListener('click', () => {
+        overlay.classList.remove('show');
+        open.classList.remove('hidde');
+    });
+}
